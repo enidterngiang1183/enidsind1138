7313ae96516cc378689805385e50f7dcbce9515a
enidterngiang1183 <enidsinclair054@gmail.com>
2026-03-10 22:25:20 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+def greet():
+    print("Hello! Welcome to my Python project")
+
+def ask_name():
+    name = input("What is your name? ")
+    print(f"Nice to meet you, {name}!")
+
+def main():
+    greet()
+    ask_name()
+
+if __name__ == "__main__":
+    main()
